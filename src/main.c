5d0d82aa6bef85a416e38991a3d66162e897d952
kurtis-lew <kurtis.a.lew@gmail.com>
2026-03-24 09:55:57 -0700

    fix: Temporarily pre-define bootloader version register

--- src/main.c
+++ src/main.c
@@ -172,7 +172,7 @@ int main(void) {
 
   // Save bootloader version to pre-defined register, retrieved by application
   // TODO move to CF2
-  BOOTLOADER_VERSION_REGISTER = (MK_BOOTLOADER_VERSION);
+  BOOTLOADER_VERSION_REGISTER = (0x454D4F);
 
   board_init();
   bootloader_init();
